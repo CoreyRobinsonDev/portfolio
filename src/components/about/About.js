3a4102e5ad4afe9796990f82d6cb0ea89b767bb8
Corey Robinson <coreyrob52@gmail.com>
2022-05-13 13:59:30 -0400

    updated favicon

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ const About = ({style, changeComponent}) => {
       <h2 className='about-title'>About</h2>
       <h1>Hi, I'm Corey Robinson</h1>
       <span className='title'>Front-End Engineer</span>
-      <p>I'm a logical and results-driven developer dedicated to building and optimizing user-focused websites. Currently switching industries, I'm very interested and passionate about everything related to computers. I'm a life long learner who stays up to date on the latest technologies and is excited to try new things.</p>
+      <p>I'm a logical and results-driven developer dedicated to building and optimizing user-focused websites. Currently switching industries, I'm very interested and passionate about everything related to computers. I'm a lifelong learner who stays up to date on the latest technologies and is excited to try new things.</p>
     <button className='back-btn' onClick={() => changeComponent('',<Portfolio style={{}} changeComponent={() => {}}></Portfolio>)}>Go Back</button>
     </div>
   </animated.section>
